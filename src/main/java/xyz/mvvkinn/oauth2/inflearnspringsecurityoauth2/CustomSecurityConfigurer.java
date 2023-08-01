f53242bdfa5754ebed2d2414e1cff263b68a90bd
mvvkinn <wo7910@naver.com>
2023-08-01 10:51:36 +0900

    [Spring Security Fundamentals] 초기화 과정 이해 - SecurityBuilder/SecurityConfigurer

---
+++ src/main/java/xyz/mvvkinn/oauth2/inflearnspringsecurityoauth2/CustomSecurityConfigurer.java
@@ -0,0 +1,30 @@
+package xyz.mvvkinn.oauth2.inflearnspringsecurityoauth2;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+
+public class CustomSecurityConfigurer extends AbstractHttpConfigurer<CustomSecurityConfigurer, HttpSecurity> {
+    private boolean isSecure;
+
+    @Override
+    public void init(HttpSecurity builder) throws Exception {
+        super.init(builder);
+        System.out.println("init method started");
+    }
+
+    @Override
+    public void configure(HttpSecurity builder) throws Exception {
+        super.configure(builder);
+        System.out.println("configure method started");
+        if(isSecure) {
+            System.out.println("HTTPS required");
+        } else {
+            System.out.println("HTTPS is optional");
+        }
+    }
+
+    public CustomSecurityConfigurer setFlag(boolean isSecure) {
+        this.isSecure = isSecure;
+        return this;
+    }
+}
---
+++ src/main/java/xyz/mvvkinn/oauth2/inflearnspringsecurityoauth2/SecurityConfig.java
@@ -0,0 +1,19 @@
+package xyz.mvvkinn.oauth2.inflearnspringsecurityoauth2;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfig  {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().authenticated();
+        http.formLogin();
+        http.apply(new CustomSecurityConfigurer().setFlag(true));
+
+        return http.build();
+    }
+}
