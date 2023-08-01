1e6d7e39b60754361974094a4a2a3773c68c9fb0
mvvkinn <wo7910@naver.com>
2023-08-01 09:37:09 +0900

    Init commit

---
+++ src/main/java/xyz/mvvkinn/oauth2/inflearnspringsecurityoauth2/InflearnSpringSecurityOauth2Application.java
@@ -0,0 +1,13 @@
+package xyz.mvvkinn.oauth2.inflearnspringsecurityoauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnSpringSecurityOauth2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InflearnSpringSecurityOauth2Application.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/mvvkinn/oauth2/inflearnspringsecurityoauth2/InflearnSpringSecurityOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.mvvkinn.oauth2.inflearnspringsecurityoauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnSpringSecurityOauth2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
